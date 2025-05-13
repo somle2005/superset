e055839065dfaf65198becc5a56e624d2dec42a9
yccnew20 <18037022725@163.com>
2025-05-13 11:02:01 +0800

    disable csrf config

--- superset/config.py
+++ superset/config.py
@@ -258,7 +258,7 @@ SQLGLOT_DIALECTS_EXTENSIONS: dict[str, Dialects | type[Dialect]] = {}
 QUERY_SEARCH_LIMIT = 1000
 
 # Flask-WTF flag for CSRF
-WTF_CSRF_ENABLED = True
+WTF_CSRF_ENABLED = False
 
 # Add endpoints that need to be exempt from CSRF protection
 WTF_CSRF_EXEMPT_LIST = [
@@ -1907,7 +1907,7 @@ class ExtraDynamicQueryFilters(TypedDict, total=False):
 
 EXTRA_DYNAMIC_QUERY_FILTERS: ExtraDynamicQueryFilters = {}
 
-WTF_CSRF_CHECK_REFERER = False
+
 print(' superset config.py load done...')
 # -------------------------------------------------------------------
 # *                WARNING:  STOP EDITING  HERE                    *
