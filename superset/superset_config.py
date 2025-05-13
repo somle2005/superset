611bc81d615e5f29384324dcf560e3d40743b9b5
yccnew20 <18037022725@163.com>
2025-05-13 10:22:34 +0800

    add webserver domains

--- superset/superset_config.py
+++
@@ -1,9 +0,0 @@
-
-WEBSERVER_DOMAINS = ["superset.somle.com", "superset.somle.com:55015"]
-
-print('superset_config.py loaded')
-SUPERSET_WEBSERVER_DOMAINS =["superset.somle.com", "superset.somle.com:55015"]
-
-
-# 可选：关闭 CSP 警告
-CONTENT_SECURITY_POLICY_WARNING = False
