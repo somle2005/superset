30616833cfb5c45033dce66f8054dd18cf079820
yccnew20 <18037022725@163.com>
2025-05-13 10:00:10 +0800

    change cross config

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -1,6 +1,9 @@
-# 允许的访问域名，解决 CSRF 问题
+
 WEBSERVER_DOMAINS = ["superset.somle.com", "superset.somle.com:55015"]
+
+print('superset_config.py loaded')
+SUPERSET_WEBSERVER_DOMAINS =["superset.somle.com", "superset.somle.com:55015"]
+
 
 # 可选：关闭 CSP 警告
 CONTENT_SECURITY_POLICY_WARNING = False
-
